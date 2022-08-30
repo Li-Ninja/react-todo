caeeca45a4821f99b2053ee967746e6acdaa741b
Ninja <lininja.tw@gmail.com>
2022-08-30 22:14:52 +0800

    feat: api maker

---
+++ src/makers/api.maker.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+const api = axios.create({
+  baseURL: 'https://todoo.5xcamp.us/',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
+
+const getApi = (url, postData) => api.get(url, postData);
+const postApi = (url, postData) => api.post(url, postData);
+const putApi = (url, postData) => api.put(url, postData);
+const deleteApi = (url, postData) => api.delete(url, postData);
+const patchApi = (url, postData) => api.patch(url, postData);
+
+export {
+  getApi,
+  postApi,
+  putApi,
+  deleteApi,
+  patchApi
+};
