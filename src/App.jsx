b909dc5e5ad88d468fbd9a031a539aa7e85eead4
Ninja <lininja.tw@gmail.com>
2022-09-03 16:15:10 +0800

    chore: eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
 import {
   HashRouter,
   Route,
-  Routes
+  Routes,
 } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
