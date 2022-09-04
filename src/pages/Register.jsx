ba6b1befd1b76d422d0268a7ad5d9a5e6f4a4b08
Ninja <lininja.tw@gmail.com>
2022-09-04 21:32:55 +0800

    feat: Register page's layout refer to codepen

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { postUser } from '../apis/user.api';
 import { email, password, required } from '../makers/rule.maker';
 import { successNotify } from '../makers/notify.maker';
+import LoginImage from '../assets/images/login.png';
+import TodoListImage from '../assets/images/online_todo_list.png';
 
 export default function Register() {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -26,98 +31,127 @@ export default function Register() {
     return handleSubmit(onValid, onInvalid);
   }
 
-  function goToLoginPage() {
+  function goToLoginPage(e) {
+    e.preventDefault();
     navigate('/Login');
   }
 
   return (
-    <div>
-      <form onSubmit={submit()}>
-        <div className="col-12">
-          <div>
-            Email
-          </div>
-          <input
-            placeholder="請輸入 Email"
-            {...register('email', {
-              ...required(),
-              ...email()
-            })}
-          />
-          <span style={{ color: 'red' }}>
-            {errors.email ? errors.email.message : ''}
-          </span>
+    <div id="signUpPage" className="bg-yellow">
+      <div className="container signUpPage vhContainer">
+        <div className="side">
+          <a><img className="logoImg" src={TodoListImage} alt="" /></a>
+          <img className="d-m-n" src={LoginImage} alt="workImg" />
         </div>
-        <div className="col-12">
-          <div>
-            您的暱稱
-          </div>
-          <input
-            placeholder="請輸入您的暱稱"
-            {...register(
-              'nickname',
-              {
-                ...required()
-              }
-            )}
-          />
-          <span style={{ color: 'red' }}>
-            {errors.nickname ? errors.nickname.message : ''}
-          </span>
-        </div>
-        <div className="col-12">
-          <div>
-            密碼
-          </div>
-          <input
-            placeholder="請輸入密碼"
-            {...register(
-              'password',
-              {
+        <div>
+          <form
+            className="formControls"
+            onSubmit={submit()}
+          >
+            <h2 className="formControls_txt">
+              註冊帳號
+            </h2>
+            <label
+              className="formControls_label"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="formControls_input"
+              type="text"
+              id="email"
+              placeholder="請輸入 email"
+              {...register('email', {
                 ...required(),
-                ...password()
-              }
-            )}
-          />
-          <span style={{ color: 'red' }}>
-            {errors.password ? errors.password.message : ''}
-          </span>
+                ...email()
+              })}
+            />
+            <span style={{ color: 'red' }}>
+              {errors.email ? errors.email.message : ''}
+            </span>
+            <label
+              className="formControls_label"
+              htmlFor="nickname"
+            >
+              您的暱稱
+            </label>
+            <input
+              className="formControls_input"
+              type="text"
+              id="nickname"
+              placeholder="請輸入您的暱稱"
+              {...register(
+                'nickname',
+                {
+                  ...required()
+                }
+              )}
+            />
+            <span style={{ color: 'red' }}>
+              {errors.nickname ? errors.nickname.message : ''}
+            </span>
+            <label
+              className="formControls_label"
+              htmlFor="password"
+            >
+              密碼
+            </label>
+            <input
+              className="formControls_input"
+              type="password"
+              id="password"
+              placeholder="請輸入密碼"
+              {...register(
+                'password',
+                {
+                  ...required(),
+                  ...password()
+                }
+              )}
+            />
+            <span style={{ color: 'red' }}>
+              {errors.password ? errors.password.message : ''}
+            </span>
+            <label
+              className="formControls_label"
+              htmlFor="passwordDoubleCheck"
+            >
+              再次輸入密碼
+            </label>
+            <input
+              className="formControls_input"
+              type="password"
+              id="passwordDoubleCheck"
+              placeholder="請再次輸入密碼"
+              {...register(
+                'passwordDoubleCheck',
+                {
+                  ...required(),
+                  ...password()
+                  // TODO password double check
+                }
+              )}
+            />
+            <span style={{ color: 'red' }}>
+              {errors.passwordDoubleCheck ? errors.passwordDoubleCheck.message : ''}
+            </span>
+            <input
+              className="formControls_btnSubmit"
+              type="submit"
+              value="註冊帳號"
+            />
+            <a
+              className="formControls_btnLink"
+              href="#"
+              onClick={goToLoginPage}
+            >
+              登入
+            </a>
+          </form>
         </div>
-        <div className="col-12">
-          <div>
-            再次輸入密碼
-          </div>
-          <input
-            placeholder="請再次輸入密碼"
-            {...register(
-              'passwordDoubleCheck',
-              {
-                ...required(),
-                ...password()
-                // TODO password double check
-              }
-            )}
-          />
-          <span style={{ color: 'red' }}>
-            {errors.passwordDoubleCheck ? errors.passwordDoubleCheck.message : ''}
-          </span>
-        </div>
-        <div className="col-12">
-          <input
-            type="submit"
-            style={{ display: 'block' }}
-            value="提交"
-          />
-        </div>
-      </form>
-      <div>
-        <input
-          type="submit"
-          style={{ display: 'block' }}
-          value="登入"
-          onClick={() => goToLoginPage()}
-        />
       </div>
+
     </div>
   );
 }
