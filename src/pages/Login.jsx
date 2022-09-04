0ce56a5bbe1e5b4497415f55d5b7f24733e50868
Ninja <lininja.tw@gmail.com>
2022-09-04 21:16:17 +0800

    feat: Login page's layout refer to codepen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -5,6 +8,8 @@ import { useNavigate } from 'react-router-dom';
 import { postSignIn } from '../apis/user.api';
 import { email, password, required } from '../makers/rule.maker';
 import { successNotify } from '../makers/notify.maker';
+import LoginImage from '../assets/images/login.png';
+import TodoListImage from '../assets/images/online_todo_list.png';
 
 export default function Login(props) {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -29,60 +34,78 @@ export default function Login(props) {
     return handleSubmit(onValid, onInvalid);
   }
 
-  function goToRegisterPage() {
+  function goToRegisterPage(e) {
+    e.preventDefault();
     navigate('/Register');
   }
   return (
-    <div>
-      <form onSubmit={submit()}>
-        <div className="col-12">
-          <div>
-            Email
-          </div>
-          <input
-            placeholder="請輸入 Email"
-            {...register('email', {
-              ...required(),
-              ...email()
-            })}
-          />
-          <span style={{ color: 'red' }}>
-            {errors.email ? errors.email.message : ''}
-          </span>
+    <div id="loginPage" className="bg-yellow">
+      <div className="container loginPage vhContainer ">
+        <div className="side">
+          <a><img className="logoImg" src={TodoListImage} alt="" /></a>
+          <img className="d-m-n" src={LoginImage} alt="workImg" />
         </div>
-        <div className="col-12">
-          <div>
-            密碼
-          </div>
-          <input
-            placeholder="請輸入密碼"
-            {...register(
-              'password',
-              {
+        <div>
+          <form
+            className="formControls"
+            onSubmit={submit()}
+          >
+            <h2 className="formControls_txt">
+              最實用的線上代辦事項服務
+            </h2>
+            <label
+              className="formControls_label"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="formControls_input"
+              id="email"
+              placeholder="請輸入 Email"
+              {...register('email', {
                 ...required(),
-                ...password()
-              }
-            )}
-          />
-          <span style={{ color: 'red' }}>
-            {errors.passwordDoubleCheck ? errors.passwordDoubleCheck.message : ''}
-          </span>
+                ...email()
+              })}
+            />
+            <span style={{ color: 'red' }}>
+              {errors.email ? errors.email.message : ''}
+            </span>
+            <label
+              className="formControls_label"
+              htmlFor="password"
+            >
+              密碼
+            </label>
+            <input
+              className="formControls_input"
+              type="password"
+              placeholder="請輸入密碼"
+              {...register(
+                'password',
+                {
+                  ...required(),
+                  ...password()
+                }
+              )}
+            />
+            <span style={{ color: 'red' }}>
+              {errors.passwordDoubleCheck ? errors.passwordDoubleCheck.message : ''}
+            </span>
+            <input
+              className="formControls_btnSubmit"
+              type="submit"
+              value="登入"
+            />
+            <a
+              className="formControls_btnLink"
+              href="#"
+              onClick={goToRegisterPage}
+            >
+              註冊帳號
+            </a>
+          </form>
         </div>
-        <div className="col-12">
-          <input
-            type="submit"
-            style={{ display: 'block' }}
-            value="登入"
-          />
-        </div>
-      </form>
-      <div>
-        <input
-          type="submit"
-          style={{ display: 'block' }}
-          value="註冊帳號"
-          onClick={() => goToRegisterPage()}
-        />
       </div>
     </div>
   );
