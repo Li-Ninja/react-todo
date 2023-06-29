a6a0adb9cdf54576310e56ba2c60e88dc87d9af9
Li-Ninja <lininja.tw@gmail.com>
2023-06-29 23:54:16 +0800

    Updates

--- src/const/index.js
+++
@@ -1,5 +0,0 @@
-export const PageTypeEnum = Object.freeze(
-  { All: 'All', Complete: 'Complete', NotComplete: 'NotComplete' }
-);
-
-export const empty = {};
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoListReducer from './slices/todoListSlice';
-
-export const store = configureStore({
-  reducer: {
-    todoList: todoListReducer
-  }
-});
